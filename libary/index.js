8f0b4b87fe677f5c941b41816d083fae771263c4
付文星 <funwenxing@sleepace.net>
2018-09-19 09:57:18 +0800

    新建

---
+++ libary/index.js
@@ -0,0 +1,7 @@
+'use strict';
+import sum from './sum'
+function fun(a, b) {
+  sum(a, b)
+}
+
+export default fun
---
+++ libary/sum.js
@@ -0,0 +1,8 @@
+'use strict';
+
+function sum(a, b) {
+  console.log("a + b = " + (a + b))
+  return a + b
+}
+
+export default sum
